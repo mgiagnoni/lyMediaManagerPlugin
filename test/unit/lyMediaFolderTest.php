9588440aa71a9d57d9c1e59c16876655bc878840
mgiagnoni <gimassimo@gmail.com>
2010-10-11 18:36:59 +0200

    Created unit tests for synchronize task.

--- test/unit/lyMediaFolderTest.php
+++ test/unit/lyMediaFolderTest.php
@@ -9,7 +9,7 @@ $root = lyMediaFolderTable::getInstance()
 
 $fs = new lyMediaFileSystem();
 
-$t = new lime_test(15, new lime_output_color());
+$t = new lime_test(30, new lime_output_color());
 
 $t->info('Create first level folder');
 $folder = new lyMediaFolder();
@@ -81,3 +81,38 @@ $folder2->refresh();
 $t->is($folder2->getRelativePath(), 'test_root/test/test-sub/test2/', '->getRelativePath()');
 $t->is($folder2->getNode()->getPath('/', true), 'test_root/test/test-sub/test2', 'Folder has right path');
 $t->ok($fs->is_dir($folder2->getRelativePath()), 'Folder exists in filesystem');
+
+$t->info('Synchronize folder');
+//Create orphaned asset
+$asset = new lyMediaAsset();
+$asset->setFolder($folder);
+$asset->setFilename(dirname(__FILE__) . '/../data/assets/asseta.png');
+$asset->save();
+
+$root->synchronizeWith(dirname(__FILE__) . '/../data/assets/',false);
+
+$asset = lyMediaAssetTable::getInstance()
+  ->findOneByFilename('asset1.png');
+$t->ok(is_object($asset), 'Asset created in root');
+$t->is($asset->getPath(), 'test_root/asset1.png', 'Asset has correct path');
+$t->ok($fs->is_file('test_root/asset1.png'), 'Asset exists in filesystem');
+$t->ok($fs->is_file('test_root/thumbs/small_asset1.png'), 'Small thumbnail exists in filesystem');
+$t->ok($fs->is_file('test_root/thumbs/medium_asset1.png'), 'Medium thumbnail exists in filesystem');
+
+$asset = lyMediaAssetTable::getInstance()
+  ->findOneByFilename('assetc.png');
+$t->ok(is_object($asset), 'Asset created in subfolder');
+$t->is($asset->getPath(), 'test_root/test/test-sub/assetc.png', 'Asset has correct path');
+$t->ok($fs->is_file('test_root/test/test-sub/assetc.png'), 'Asset exists in filesystem');
+$t->ok($fs->is_file('test_root/test/test-sub/thumbs/small_assetc.png'), 'Small thumbnail exists in filesystem');
+$t->ok($fs->is_file('test_root/test/test-sub/thumbs/medium_assetc.png'), 'Medium thumbnail exists in filesystem');
+$t->ok($fs->is_dir('test_root/test/test-sub/test2'), 'Orphaned folder still exists');
+$t->ok($fs->is_file('test_root/test/asseta.png'), 'Orphaned asset still exists');
+
+$t->info('Synchronize folder (remove orphaned folder)');
+$root->synchronizeWith(dirname(__FILE__) . '/../data/assets/',false,false,true);
+$t->ok(!$fs->is_dir('test_root/test/test-sub/test2'), 'Orphaned folder no longer exists');
+$t->ok($fs->is_file('test_root/test/asseta.png'), 'Orphaned asset still exists');
+$t->info('Synchronize folder (remove orphaned asset)');
+$root->synchronizeWith(dirname(__FILE__) . '/../data/assets/',false,true,false);
+$t->ok(!$fs->is_file('test_root/test/asseta.png'), 'Orphaned asset no longer exists');
