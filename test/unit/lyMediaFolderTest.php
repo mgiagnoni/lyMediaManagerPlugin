d59e43a03598fd03f69daa011b4bd48803f765c4
mgiagnoni <gimassimo@gmail.com>
2010-10-11 13:28:25 +0200

    Added some comments to lyMediaFolder unit test.

--- test/unit/lyMediaFolderTest.php
+++ test/unit/lyMediaFolderTest.php
@@ -17,6 +17,10 @@ $folder->setName('test');
 $folder->create($root);
 $folder->refresh();
 
+/*
+ * test_root
+ *  -- test
+ */
 $t->is($folder->getName(), 'test', '->getName()');
 $t->is($folder->getRelativePath(), 'test_root/test/', '->getRelativePath()');
 $t->ok($folder->getNode()->isValidNode(), 'Folder is a valid node');
@@ -29,6 +33,11 @@ $sub->setName('test-sub');
 $sub->create($folder);
 $sub->refresh();
 
+/*
+ * test_root
+ *  -- test
+ *  -- -- test-sub
+ */
 $t->is($sub->getRelativePath(), 'test_root/test/test-sub/', '->getRelativePath()');
 $t->ok($sub->getNode()->isValidNode(), 'Folder is a valid node');
 $t->is($sub->getNode()->getPath('/', true), 'test_root/test/test-sub', 'Subfolder has right path');
@@ -39,10 +48,22 @@ $folder2->setName('test2');
 $folder2->create($root);
 $folder2->refresh();
 
+/*
+ * test_root
+ *  -- test
+ *  -- -- test-sub
+ *  -- test 2
+ */
 $t->info('Move folder');
 $folder2->move($folder);
 $folder2->refresh();
 
+/*
+ * test_root
+ *  -- test
+ *  -- -- test 2
+ *  -- -- test-sub
+ */
 $t->is($folder2->getRelativePath(), 'test_root/test/test2/', '->getRelativePath()');
 $t->is($folder2->getNode()->getPath('/', true), 'test_root/test/test2', 'Folder has right path');
 $t->ok($fs->is_dir($folder2->getRelativePath()), 'Folder exists in filesystem');
@@ -51,6 +72,12 @@ $t->info('Move folder deeper');
 $folder2->move($sub);
 $folder2->refresh();
 
+/*
+ * test_root
+ *  -- test
+ *  -- -- test-sub
+ *  -- -- -- test 2
+ */
 $t->is($folder2->getRelativePath(), 'test_root/test/test-sub/test2/', '->getRelativePath()');
 $t->is($folder2->getNode()->getPath('/', true), 'test_root/test/test-sub/test2', 'Folder has right path');
 $t->ok($fs->is_dir($folder2->getRelativePath()), 'Folder exists in filesystem');
